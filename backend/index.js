f109d054fc8a8ed6ce876014d7028a5bd9954bd5
Andrew Lawrence <and.p.law@gmail.com>
2025-05-28 22:00:53 +0200

    add const PORT

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const PORT = process.env.PORT || 4000; //
 
 const app = express();
 const server = http.createServer(app);
